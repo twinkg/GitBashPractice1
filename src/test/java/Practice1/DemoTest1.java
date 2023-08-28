8e02870074d25217274828f06bb3d5cb18c82ec2
twinkg <twinklegajbhiye@gmail.com>
2023-08-28 07:40:05 +0530

    pushone

---
+++ src/test/java/Practice1/DemoTest1.java
@@ -0,0 +1,16 @@
+package Practice1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	WebDriver driver = new ChromeDriver();
+	
+	@Test
+	public void D1Test1()
+	{
+		System.out.println("world");
+	}
+
+}
